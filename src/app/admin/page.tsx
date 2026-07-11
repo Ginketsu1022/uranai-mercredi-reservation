39656b2477e45e0884d812534cb9b509d2c250f7
Ginketsu1022 <hiku5920@gmail.com>
2026-07-12 02:39:20 +0900

    restore admin auth

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,22 @@
+import { redirect } from "next/navigation";
 import { auth } from "@/auth";
+import AdminPage from "./AdminPage";
 
 export default async function Page() {
   const session = await auth();
 
-  return (
-    <pre>
-      {JSON.stringify(session, null, 2)}
-    </pre>
-  );
+  if (!session?.user) {
+    redirect("/");
+  }
+
+  const admins = [
+    "sob_gear_2@yahoo.co.jp",
+    "chii.hono.official@gmail.com",
+  ];
+
+  if (!admins.includes(session.user.email ?? "")) {
+    redirect("/");
+  }
+
+  return <AdminPage />;
 }
