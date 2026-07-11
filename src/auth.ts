45e5b115141ba042ff2104013f05fbda8d965d07
Ginketsu1022 <hiku5920@gmail.com>
2026-07-12 01:51:16 +0900

    Auth exports cleanup

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+export const { handlers, auth } = NextAuth({
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
